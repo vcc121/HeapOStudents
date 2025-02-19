163ac1bc339b0e6166f7733b2b9507df4e4f0151
vcc121 <vaughn.cox@bsu.edu>
2025-02-19 15:08:37 +0000

    finished address h file

--- address.h
+++ address.h
@@ -1,4 +1,16 @@
+#ifndef ADDRESS_H_EXISTS
+#define ADDRESS_H_EXISTS
+
 class Address {
 	private:
-		
+		std::string street;
+		std::string town;
+		std::string state;
+		std::string zipCode;
+	public:
+		Address();
+		void init(std::string street, std::string town, std::string state, std::string zipCode);
+		void printAddress();
+};
 
+#endif
