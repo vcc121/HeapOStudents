52a6f398102b3ffc64a6ea160be8eb204edb4d21
vcc121 <vaughn.cox@bsu.edu>
2025-02-21 14:41:53 +0000

    working on dte

--- date.cpp
+++ date.cpp
@@ -8,4 +8,19 @@ Date::Date(){
 	dateString = "";
 }
 
-void Date::init(std::dateString)
+void Date::init(std::dateString){
+
+	std::string tDay;
+	std::string tMonth;
+	std::string tYear;
+
+	getline(ss, tDay, '/');
+	getline(ss, tMonth, '/');
+	getline(ss, tYear);
+
+	ss.clear();
+	ss.str("");
+	
+	ss << tDay << " " << tMonth << " " << tYear;
+    ss >> day >> month >> year;
+}
