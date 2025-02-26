52ee3a828ddded9163b91c1dca46da87d2326f64
vcc121 <vaughn.cox@bsu.edu>
2025-02-26 15:50:27 +0000

    working on main

--- main.cpp
+++ main.cpp
@@ -1,6 +1,28 @@
 #include <iostream>
+#include <algorithm>
+#include <vector>
 #include "date.h"
 #include "address.h"
 #include "student.h"
 
+void findStudent(std::vector<Student*>& students) {
+    std::string target;
+    bool notFound = true;
+    std::cout << "last name of student: ";
+    getline(std::cin, target);
 
+    for (Student* s : students) {
+        std::string lName = s->getLastFirst();
+        if (lName.find(target) != std::string::npos) {
+            s->printStudent();
+            notFound = false;
+        }
+    }
+
+    if (notFound) {
+        std::cout << "No students found with that last name.\n";
+    }
+}
+
+
+}
