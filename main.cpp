95a2a1e27573a933fcf5ccdfc43896ea96b060d5
vcc121 <vaughn.cox@bsu.edu>
2025-02-28 15:49:41 +0000

    working on load students function

--- main.cpp
+++ main.cpp
@@ -5,6 +5,24 @@
 #include "address.h"
 #include "student.h"
 
+void loadStudents(std::vector<Student*>& students);
+void printStudents(std::vector<Student*>& students);
+void showStudentNames(std::vector<Student*>& students);
+void findStudent(std::vector<Student*>& students);
+void delStudents(std::vector<Student*>& students);
+std::string menu();
+
+void loadStudents(std::vector<Student*>& students) {
+    std::ifstream file("students.csv"); 
+    std::string line;
+    while (std::getline(file, line)) {
+        Student* student = new Student(line);
+        students.push_back(student);
+    }
+    
+    file.close();
+}
+
 void findStudent(std::vector<Student*>& students) {
     std::string target;
     bool notFound = true;
@@ -23,6 +41,6 @@ void findStudent(std::vector<Student*>& students) {
         std::cout << "No students found with that last name.\n";
     }
 }
-
-
-}
+ bool compFirst(Student* a, Student* b){
+    return a->getFirstName() < b->getFirstName();
+ }
